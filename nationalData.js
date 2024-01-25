b9653b0d72c64b16f9c17ffc76e53163ec12b455
Sandhya Kaluram Arude <156443901+Sandhya-0407@users.noreply.github.com>
2024-01-25 10:32:36 +0530

    Add files via upload

---
+++ nationalData.js
@@ -0,0 +1,77 @@
+import manali from './national_images/manali.jpg'
+import kerla from './national_images/kerla.jpg'
+import aasam from './national_images/aasam.jpg'
+import mumbai from './national_images/mumbai.jpg'
+
+
+const nationaltour = [
+    {
+        id: 1,
+    place:"Manali",
+        price: "50,000",
+        specification: {
+            stay: " Hotel - Taj Hotel",
+            duration: "3 day 2 night",
+            destination:" Nehru kund,Jogni waterfall,Naggar castle,Bhrigu lake,jogini falls",
+            high_light:"7 places"
+
+        },
+        image:manali
+
+
+    },
+
+    {
+        id: 2,
+    place:"Kerla",
+        price: "70,000",
+        specification: {
+            stay: "hotel - taj cape town",
+            duration: "6 day 5 night",
+            destination:" Kochi,Munnar,Alappuzha,Wayand,Kovalam",
+            high_light:"5 places"
+        },
+        image:kerla
+
+    },
+
+    {
+        id: 3,
+    place:"Aasam",
+        price: "40,000",
+        specification: {
+            stay: "hotel - taj hotel",
+            duration: "4 day 3 night",
+            destination:" Guwahati,Dibrugarh,Tezpur",
+            high_light:"3 places"
+
+        },
+        image:aasam
+
+    },
+
+    {
+        id: 4,
+    place:"Mumbai",
+        price: "43,000",
+        specification: {
+            stay: "hotel - The Peninsula Paris",
+            duration: "4 day 2 night",
+            destination:"Nariman point,Kanheri caves,Elephant caves Colaba ",
+            high_light:"5 places"
+
+        },
+        image:mumbai
+
+    },
+
+
+
+
+
+
+
+
+]
+
+export default nationaltour;
